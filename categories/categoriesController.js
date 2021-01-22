1ce15a311960b1a6d9113f3d8e04fb8b1f79fcce
Alysson Eduardo <alysson6801@hotmail.com>
2021-01-21 23:50:50 -0300

    (21/01) Formulário de edição.

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -13,7 +13,7 @@ router.post("/categories/save", (req, res)=>{
     if (title != undefined) Category.create({
             title: title,
             slug: slugify(title)
-    }).then(() => res.redirect("/"))
+    }).then(() => res.redirect("/admin/categories"))
     else res.redirect("/admin/categories/new")
 })
 
@@ -41,6 +41,18 @@ router.post("/categories/delete", (req, res)=>{
     } else {
         res.redirect("/admin/categories")
     }
+})
+
+router.get("/admin/categories/edit/:id", (req, res)=>{
+    let id = req.params.id
+    if (isNaN(id)) res.redirect("/admin/categories")
+    Category.findByPk(id).then(category => {
+        if (category != undefined){
+            res.render("admin/categories/edit", {category: category})
+        } else {
+            res.redirect("/admin/categories")
+        }
+    }).catch(err => res.redirect("/admin/categories"))
 })
 
 module.exports = router
