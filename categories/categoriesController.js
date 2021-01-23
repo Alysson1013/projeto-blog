465ebaad643c1f5f9d05ac24d2ec615a9ba8a499
Alysson Eduardo <alysson6801@hotmail.com>
2021-01-22 22:31:31 -0300

    (22/01) Salvando atualização.

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -55,4 +55,20 @@ router.get("/admin/categories/edit/:id", (req, res)=>{
     }).catch(err => res.redirect("/admin/categories"))
 })
 
+router.post("/categories/update", (req, res) => {
+    let id = req.body.id
+    let title = req.body.title
+    let slug = req.body.slug
+
+    Category.update({
+        title: title,
+        slug: slugify(title)
+        
+    },{
+        where: {
+            id: id
+        }
+    }).then(()=> res.redirect("/admin/categories"))
+})
+
 module.exports = router
