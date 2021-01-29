d0de70f110c65bb94f77b762b49a2fd3f5d37eeb
Alysson Eduardo <alysson6801@hotmail.com>
2021-01-28 23:35:18 -0300

    (28/01) Sistema de Pagínação no back-end.

--- articles/articlesControllers.js
+++ articles/articlesControllers.js
@@ -79,5 +79,34 @@ router.post("/articles/update", (req, res)=>{
     }).then(() => res.redirect("/admin/articles") )
 })
 
+router.get("/articles/page/:num", (req, res)=>{
+    let page = req.params.num
+    let offset = 0
+
+    if(isNaN(page) || page == 1){
+        offset = 0
+    } else {
+        offset = (parseInt(page) - 1) * 4
+    }
+
+    Article.findAndCountAll({
+        limit: 4,
+        offset: offset
+    }).then(articles => {
+
+        let next
+
+        if(offset + 4 >= articles.count) next = false
+        else next = true
+
+        let result = {
+            next: next,
+            articles : articles,
+
+        }
+
+        res.json(result)
+    })
+})
 
 module.exports = router
